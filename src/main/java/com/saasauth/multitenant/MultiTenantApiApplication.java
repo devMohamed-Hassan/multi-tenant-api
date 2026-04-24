33d3fa1e03f8ffb7ab97b3213e9d942d3aa942d6
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2026-04-25 01:42:06 +0300

    initial setup: Spring Boot multi-tenant auth API

---
+++ src/main/java/com/saasauth/multitenant/MultiTenantApiApplication.java
@@ -0,0 +1,13 @@
+package com.saasauth.multitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantApiApplication.class, args);
+	}
+
+}
